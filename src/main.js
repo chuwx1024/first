c6b7670a7b2ce49e3944d6f23dca7bb33728d633
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-03-31 18:16:20 +0800

    importElement

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({

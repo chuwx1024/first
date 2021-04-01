81b3cd23f8c1fe4dafd800c6563eb0318f98084c
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-04-01 10:42:04 +0800

    make logincard center

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="login">
-    登录
+    <el-card class="login-card">
+    </el-card>
   </div>
 </template>
+
+<style scoped lang="less">
+.login {
+  background: url("../../assets/img/login_bg.jpg") no-repeat;
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+.login-card {
+    width: 446px;
+    height: 300px;
+    // position: absolute;
+    // left: 50%;
+    // top: 50%;
+    // transform: translate(-50%, -50%);
+    // top: 0;
+    // left: 0;
+    // right: 0;
+    // bottom: 0;
+    // margin: auto;
+  }
+}
+
+</style>

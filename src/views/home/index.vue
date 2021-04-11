9a21b9c386471279c8efc3fa77a501028fb192ef
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-04-11 18:55:20 +0800

    table数据绑定,table只能渲染文本,其他需要自定义

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -41,24 +41,28 @@
         <span>共找到? 条符合条件的内容</span>
       </div>
       <el-table
-        :data="tableData"
+        :data="Articles"
         style="width: 100%">
+        <!-- table 只能渲染文本,渲染其他的,需要自定义---- -->
         <el-table-column
-          prop="date"
+          prop="cover"
           label="封面"
           width="180">
+          <template slot-scope="scope">
+            <img :src="scope.row.cover.images[0]" alt="">  
+          </template>  
         </el-table-column>
         <el-table-column
-          prop="name"
+          prop="title"
           label="标题"
           width="180">
         </el-table-column>
         <el-table-column
-          prop="address"
+          prop="status"
           label="状态">
         </el-table-column>
         <el-table-column
-          prop="address"
+          prop="pubdate"
           label="发布日期">
         </el-table-column>
         <el-table-column
@@ -82,41 +86,45 @@ export default {
         end_pubdate: ''
       },
       rangedate: '',
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      },
-      {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      },
-      {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      },
-      {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
+      Articles: [{
+        cover: {
+          images: ['https://gimg2.baidu.com/image_search/src=http%3A%2F%2Finews.gtimg.com%2Fnewsapp_match%2F0%2F11302429824%2F0.jpg&refer=http%3A%2F%2Finews.gtimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620730162&t=13f8214d3399138b38704af7b2d260de'],
+          type: 1
+        },
+        id: 1195253183874596900,
+        pubdate: '2019-11-15 16:12:20',
+        status: 2,
+        title: '我是公主'
       }]
     }
   },
   created () {
-    const token = window.localStorage.getItem('user-token')
-    this.$axios({
-      method: 'get',
-      url: '/articles',
-      headers: {
-        Authorization: `Bearer ${token}`
+    this.fakeArticles()
+    this.loadArticles()
+
+  },
+  methods: {
+    fakeArticles () {
+      for (var i = 0; i < 10; i++) {
+        this.Articles.push(this.Articles[0])
+        console.log(this.Articles)
       }
-    }).then(res => {
-      console.log(res)
-    }).catch(err => {
-      console.log(err)
-    })
+    },
+    loadArticles () {
+      const token = window.localStorage.getItem('user-token')
+      this.$axios({
+        method: 'get',
+        url: '/articles',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+
+    }
   }
 
 }
@@ -131,5 +139,8 @@ export default {
 }
 .firstCard {
   margin-bottom: 14px;
+}
+img {
+  width: 50%;
 }
 </style>

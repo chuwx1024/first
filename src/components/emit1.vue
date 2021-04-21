fe0bacb04229a7aeec66e3b12e419769db12d134
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-04-21 10:55:52 +0800

    数据改变监听

---
+++ src/components/emit1.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sbox">
+    <p>子组件2</p>
+    <button @click="onChangeNumber(-1)">-</button>
+    <input type="text" :value="value">
+    <button @click="onChangeNumber(1)">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: ['value'],
+  data () {
+    return {
+
+    }
+  },
+  computed: {},
+  watch: {
+    value (currentdata, olddata) {
+      this.$emit('change', currentdata, olddata)
+    }
+  },
+  created () {},
+  methods: {
+    onChangeNumber (data) {
+      const newdata = this.value + data
+      this.$emit('input', newdata)
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="less">
+</style>
--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -3,15 +3,18 @@
     <p>父组件</p>
     {{ num }}
     <ButtonOption :value="num" @input="num = $event"></ButtonOption>
+    <ButtonO :value="num" @input="num = $event" @change="onChangeNumber"></ButtonO>
   </div>
 </template>
 
 <script>
 import ButtonOption from '@/components/emit.vue'
+import ButtonO from '@/components/emit1.vue'
 export default {
   name: '',
   components: {
-    ButtonOption
+    ButtonOption,
+    ButtonO
   },
   props: {},
   data () {
@@ -22,10 +25,18 @@ export default {
   computed: {},
   watch: {},
   created () {},
-  methods: {}
+  methods: {
+    onChangeNumber (a, b) {
+      console.log(a, b)
+    }
+  }
 }
 
 </script>
 
 <style scoped lang="less">
+.sbox {
+  border: 1px solid orange;
+  margin-top: 50px;
+}
 </style>

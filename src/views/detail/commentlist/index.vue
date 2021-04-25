91269469464681062f7a1f05dd45f4e2b8440472
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-04-25 10:48:10 +0800

    获取评论数据

--- src/views/detail/commentlist/index.vue
+++ src/views/detail/commentlist/index.vue
@@ -1,23 +1,81 @@
 <template>
   <div class="commentlist">
-    评论hehe
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <el-button type="text">返回全部图文</el-button>
+      </div>
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'CommentList',
   components: {},
   props: [],
   data () {
     return {
-
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   },
   computed: {},
   watch: {},
-  created () {},
-  methods: {}
+  created () {
+    this.loadComments()
+  },
+  methods: {
+    loadComments () {
+      this.$axios({
+        method: 'get',
+        url: '/comments',
+        params: {
+          type: 'a',
+          source: this.$route.params.articleid
+        }
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        this.$message({
+          type: 'error',
+          message: err
+        })
+      })
+    }
+  }
 }
 
 </script>
